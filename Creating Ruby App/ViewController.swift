1ea3ba82932a6ee28c1df5eaf40952bf1df1dfaa
Yuto Mori <ymori_0323@yahoo.co.jp>
2019-02-09 21:48:28 +0900

    Initial Commit

---
+++ Creating Ruby App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Creating Ruby App
+//
+//  Created by 森勇人 on 2019/02/09.
+//  Copyright © 2019 yuto mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
