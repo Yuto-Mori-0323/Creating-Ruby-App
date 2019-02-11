3a7476f8adaf5c422bed8110cd35483ec914fd7d
Yuto Mori <ymori_0323@yahoo.co.jp>
2019-02-11 23:56:37 +0900

    結果表示機能実装

--- Creating Ruby App/ViewController.swift
+++ Creating Ruby App/ViewController.swift
@@ -7,20 +7,51 @@
 //
 
 import UIKit
+var test = ""
 
 class ViewController: UIViewController,XMLParserDelegate {
     
-    var kanaList = [String]()
-    var kana02 = [String]()
-    var Surface = ""
     var judgeFlag = 0
+    var Surface = ""
     var Furigana = ""
+    var furigana = ""
     var furiganaList = [String]()
+    
+    @IBOutlet weak var Results: UITextView!
+    
+    @IBOutlet weak var Execution: UIButton!
+    @IBAction func Execution(_ sender: Any) {
+ /*        Creating_Ruby({ furigana in
+            print("furigana:\(furigana)")
+            self.Results.text = furigana
+        })
+         */   hogeC()
+        print("ここで終わり")
+        
+    }
+    typealias CompletionClosure = ((_ result:String) -> Void)
+    func hogeC() {
+        
+        // ①
+        Creating_Ruby(completionClosure: { (result:String) in
+            // ⑥ 10が返ってくる
+            print("result\(result)")
+            // メインスレッドからUIを更新
+            DispatchQueue.main.async {
+                self.Results.text = result
+           //     self.Results.sizeToFit()
+            }
+            
+        })
+        
+        // ④
+    }
     
     @IBOutlet weak var textField: UITextField!
     
     
-    @IBAction func Creating_Ruby(_ sender: Any) {
+    
+    func Creating_Ruby(completionClosure:@escaping CompletionClosure)  {
         
         let yahooID = "dj00aiZpPWc1ZHljbjRvNVNLSCZzPWNvbnN1bWVyc2VjcmV0Jng9NTg-"
         let grade = "1"
@@ -37,128 +68,65 @@ class ViewController: UIViewController,XMLParserDelegate {
         guard let url = URL(string: encoding_text) else{
             return
         }
-
-        print("url\(url)")
-
         let request = URLRequest(url: url)
         let session = URLSession.shared
-        let task = session.dataTask(with: request){
-            (data:Data?,response:URLResponse?, error:Error?) in
-            
+        let task = session.dataTask(with: request)
+        {(data:Data?,response:URLResponse?, error:Error?)  in  //ここからクロージャ
             guard let xmldata = data else{
                 print("データなし")
                 return
             }
-        //    print("data01\(data01)")
+            //取得したXmlの解析
+            let xmlanalysis = XmlAnalysis()
+            let furigana = xmlanalysis.analysis(xmldata:xmldata)
+            completionClosure(furigana)
             
-        //    let parser : XMLParser = XMLParser(contentsOf: data01)!
-            let parser = XMLParser(data: xmldata)
-            parser.delegate = self
-            parser.parse()
-            
-        
-        /*    if let value = String(data: data01, encoding: String.Encoding.utf8){
-                print ("value\(value)")
-            } */
         }
- 
+        print("task.resume実行前")
         task.resume()
-    }
-    
-    // XML解析開始時に実行されるメソッド
-    func parserDidStartDocument(_ parser: XMLParser) {
-        print("XML解析開始しました")
-    }
-    
-    // 解析中に要素の開始タグがあったときに実行されるメソッド
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
-        if  judgeFlag != 9{
-            switch elementName {
-            case "Surface":
-                judgeFlag = 1
-            case "Furigana":
-                judgeFlag = 2
-            case "SubWordList":
-                judgeFlag = 9
-            default:
-                break
-            }
-        }
-    }
-    
-    /*func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        for parsedStr in attributeDict {
-        //    let exchangeRate = ExchangeRate()
-            print("parsedStr\(parsedStr)")
-            switch parsedStr.key {
-            case "name":
-                kanaList.append(parsedStr.value)
-            default: break
-            }
-        }
-        print("kanaList\(kanaList)")
-    } */
-    
-    // 開始タグと終了タグでくくられたデータがあったときに実行されるメソッド
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
-        //print("要素:" + string)
-        
-        switch judgeFlag {
-        case 1:
-            Surface = string
-            judgeFlag = 3
-        case 2:
-            Furigana = string
-            judgeFlag = 4
-        default:
-            break
-        }
-    }
-    
-    // 解析中に要素の終了タグがあったときに実行されるメソッド
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        //print("終了タグ:" + elementName)
-        if elementName == "Word" {
-            switch judgeFlag {
-            case 3:
-                furiganaList.append(Surface)
-            case 4:
-                furiganaList.append(Furigana)
-            case 9:
-                furiganaList.append(Furigana)
-                judgeFlag = 0
-            default:
-                break
-            }
-            judgeFlag = 0
-        }
-    }
-    
-    // XML解析終了時に実行されるメソッド
-    func parserDidEndDocument(_ parser: XMLParser) {
-        print("XML解析終了しました")
-        print("furiganaList\(furiganaList)")
-    }
-    
-    
-    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
-        print(parseError)
-    }
-    
-    
-    
-    @IBAction func comeHome (segue: UIStoryboardSegue){
+        print("task.resume実行後")
         
     }
     
+    
+   
+    
+    
+    
+    
+    
+  //  @IBAction func comeHome (segue: UIStoryboardSegue){
+        
+  //  }
     
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 枠のカラー
+        Results.layer.borderColor = UIColor.gray.cgColor
+        
+        // 枠の幅
+        Results.layer.borderWidth = 0.3
+        
+        let rgba = UIColor(red: 255/255, green: 128/255, blue: 168/255, alpha: 1.0) // ボタン背景色設定
+        Execution.backgroundColor = rgba                                               // 背景色
+        Execution.layer.borderWidth = 0.3                                              // 枠線の幅
+        Execution.layer.borderColor = UIColor.black.cgColor                            // 枠線の色
+        Execution.layer.cornerRadius = 5.0// 角丸のサイズ
+        Execution.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        
+        
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //override func viewDidDisappear(_ animated: Bool) {
+    //    send_furigana = furigana
+        
+        
+    //}
 
 
 }
